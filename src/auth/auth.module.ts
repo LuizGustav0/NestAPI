d36b3530b99f7b146f741898d30a1567427f0a8c
Luiz Gustavo <contato-luiz-gustavo@outlook.com>
2023-11-28 13:32:46 -0300

    feat: create module auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,19 @@
 import { Module } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
+import { AuthController } from "./auth.controller";
+import { UserModule } from "src/user/user.module";
+import { UserService } from "src/user/user.service";
+import { PrismaModule } from "src/prisma/prisma.module";
 
 @Module({
     imports: [JwtModule.register({
         secret: "R]k&2yZ£M@g!X6{^SC7uAUH,|:H=i|0&"
-    })]
+    }), 
+    UserModule,
+    PrismaModule
+],
+    controllers: [AuthController],
+    exports: [UserService]
 
 })
 export class AuthModule {
