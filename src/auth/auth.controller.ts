54722b448cfd9465083919e423b27d83280f8245
Luiz Gustavo <contato-luiz-gustavo@outlook.com>
2023-11-30 12:27:27 -0300

    feat: add decorator user in auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,6 +7,7 @@ import { UserService } from "src/user/user.service";
 import { AuthService } from "./auth.service";
 import { AuthMeDTO } from "./dto/auth-me.dto";
 import { AuthGuard } from "src/guards/auth.guard";
+import { User } from "src/decorators/user.decorator";
 
 @Controller('auth')
 export class AuthController {
@@ -40,7 +41,7 @@ export class AuthController {
 
     @UseGuards(AuthGuard)
     @Post('me')
-    async me(@Req() req) {
-        return {me: "OK", data: req.tokenPayload}
+    async me(@User() user) {
+        return {user}
     }
 }
