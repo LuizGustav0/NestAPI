b374184e19b774bced866c1dbe56409c0161bf89
Luiz Gustavo <contato-luiz-gustavo@outlook.com>
2023-11-30 12:27:42 -0300

    feat: add aguard

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -1,14 +1,18 @@
 import {  CanActivate, Injectable, ExecutionContext } from "@nestjs/common";
 import { AuthService } from "src/auth/auth.service";
+import { UserService } from "src/user/user.service";
 
 
 @Injectable()
 export class AuthGuard implements CanActivate {
 
-    constructor(private readonly authService: AuthService){}
+    constructor(
+        private readonly authService: AuthService,
+        private readonly userService: UserService
+    ){}
 
 
-    canActivate(context: ExecutionContext){
+    async canActivate(context: ExecutionContext){
         const request  = context.switchToHttp().getRequest();
         const {authorization} = request.headers;
        
@@ -16,6 +20,8 @@ export class AuthGuard implements CanActivate {
             const data = this.authService.checkToken((authorization ?? "").split(" ")[1])
 
             request.tokenPayload = data;
+
+            request.user = await this.userService.show(data.id)
 
             return true
         } catch (error) {
