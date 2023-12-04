28a7a35d0e7df0f5430975596306c0eeafab3b0c
Luiz Gustavo <contato-luiz-gustavo@outlook.com>
2023-12-04 12:33:24 -0300

    feat: enabled cors

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableShutdownHooks();
-
+  app.enableCors();
   app.useGlobalPipes(new  ValidationPipe())
-
 
   // Interceptor global
   //app.useGlobalInterceptors(new LogInterceptor) 
