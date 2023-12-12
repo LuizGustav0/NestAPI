110377e69e118e990b13e513e7cd237dac036563
Luiz Gustavo <contato-luiz-gustavo@outlook.com>
2023-12-12 12:50:37 -0300

    feat: add mailer module app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,9 @@ import { AuthModule } from './auth/auth.module';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { PugAdapter } from '@nestjs-modules/mailer/dist/adapters/pug.adapter';
+
 
 @Module({
   imports: [
@@ -15,7 +18,27 @@ import { ConfigModule } from '@nestjs/config';
       limit: 100
     }]),
     forwardRef(() => UserModule), 
-    forwardRef(() => AuthModule)
+    forwardRef(() => AuthModule),
+    MailerModule.forRoot({
+      transport: {
+        host: 'smtp.ethereal.email',
+        port: 587,
+        auth: {
+          user: 'lottie.adams4@ethereal.email',
+          pass: 'WUyV8cjzv1TnTxd8dS'
+        }
+      }, //utilizado fake criado em https://ethereal.email/create
+      defaults: {
+        from: '"teste email" <lottie.adams4@ethereal.email>',
+      },
+      template: {
+        dir: __dirname + '/templates',
+        adapter: new PugAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    }),
   ],
   controllers: [AppController],
   providers: [AppService, {
